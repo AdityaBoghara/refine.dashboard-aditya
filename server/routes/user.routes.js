34fee4fd2b66d14c54f6d5ee9fd8a3cb7c4e98e3
AdityaBoghara <bogharaaditya@gmail.com>
2023-02-27 15:43:49 -0700

    Commit on 3:43 27th feb

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,1 +1,12 @@
-import { express } from "express";
+import { express, Router } from "express"; 
+import { createUser, getAllUsers, getUserInfoByID } from "../controllers/user.controller";
+// import all the controllers
+
+const router = express.Router();
+
+router.route('/').get(getAllUsers)
+router.route('/').post(createUser)
+router.route('/:id').get(getUserInfoByID)
+
+export default router
+
